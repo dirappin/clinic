374a16e5c8fc9f538aff26b120baaeb50fb32e62
crystal <crystal@rollspace.com>
2024-01-16 21:55:14 +0300

    implemented searching technic

--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -12,7 +12,6 @@ import { IoSearchOutline } from "react-icons/io5";
 import axios from "axios";
 import { backendBaseUrl } from "../constant";
 import { MdClose } from "react-icons/md";
-import { PiFolderOpenThin } from "react-icons/pi";
 import EmptyResult from "./common/EmptyResult";
 
 const thclass = "text-start text-sm font-medium py-3 px-2 whitespace-nowrap";
